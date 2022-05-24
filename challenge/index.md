d17e590d575bd8bfcda8ee68c5413474a49be2e1
A.J. Krumholz <krumholz.alan@gmail.com>
2022-05-23 22:17:37 -0400

    Complete timed exercise

---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+# Book Titles
+
+1. Harry Potter
+2. Moby Dick
+3. 1,000 Years of Solitude
+4. What Happened to You?
+5. Lord of the Rings
+
+I (A.J.) drove while Ashley & Tom navigated. This was helpful because I couldn't see the lesson plan very well. The tip to not focus excessively on git was helpful in pushing us along.
+
+We didn't have to look anything up and we managed our time well.
---
+++ challenge/library.rb
@@ -0,0 +1,21 @@
+# This is all about library stuff
+
+# Arguably the only work by Herman Melville anyone knows
+#This variable stores the title of a book as a string
+book_title = "Moby Dick"
+
+# There's a lot of titles because it's a library
+# This variable is storing an integer equal to the number of books in the library
+number_of_titles = 54425
+
+# This variable defines whether or not shouting is allowed as a boolean value
+shouting_allowed = false
+
+number_of_pages = 435
+
+# This variable stores a string that contains the physical address of the library
+library_address = "123 Library St."
+
+library_name = "Library St. Library"
+
+favorite_books = ["The Adventures of Kavalier and Klay", "The Good Earth", "The Perks of Being a Wallflower", "The Dictionary"]
